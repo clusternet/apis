qmhu/clusternet

auto generated files

pkg/apis/apps/v1alpha1/zz_generated.deepcopy.go
@@ -25,6 +25,89 @@ import (
 )
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *Base) DeepCopyInto(out *Base) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	in.Spec.DeepCopyInto(&out.Spec)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new Base.
+func (in *Base) DeepCopy() *Base {
+	if in == nil {
+		return nil
+	}
+	out := new(Base)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *Base) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *BaseList) DeepCopyInto(out *BaseList) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
+	if in.Items != nil {
+		in, out := &in.Items, &out.Items
+		*out = make([]Base, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new BaseList.
+func (in *BaseList) DeepCopy() *BaseList {
+	if in == nil {
+		return nil
+	}
+	out := new(BaseList)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *BaseList) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *BaseSpec) DeepCopyInto(out *BaseSpec) {
+	*out = *in
+	if in.Feeds != nil {
+		in, out := &in.Feeds, &out.Feeds
+		*out = make([]Feed, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new BaseSpec.
+func (in *BaseSpec) DeepCopy() *BaseSpec {
+	if in == nil {
+		return nil
+	}
+	out := new(BaseSpec)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *ChartReference) DeepCopyInto(out *ChartReference) {
 	*out = *in
 	return
@@ -36,6 +119,90 @@ func (in *ChartReference) DeepCopy() *ChartReference {
 		return nil
 	}
 	out := new(ChartReference)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ClusterLocalization) DeepCopyInto(out *ClusterLocalization) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	in.Spec.DeepCopyInto(&out.Spec)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ClusterLocalization.
+func (in *ClusterLocalization) DeepCopy() *ClusterLocalization {
+	if in == nil {
+		return nil
+	}
+	out := new(ClusterLocalization)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *ClusterLocalization) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ClusterLocalizationList) DeepCopyInto(out *ClusterLocalizationList) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
+	if in.Items != nil {
+		in, out := &in.Items, &out.Items
+		*out = make([]ClusterLocalization, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ClusterLocalizationList.
+func (in *ClusterLocalizationList) DeepCopy() *ClusterLocalizationList {
+	if in == nil {
+		return nil
+	}
+	out := new(ClusterLocalizationList)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *ClusterLocalizationList) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ClusterLocalizationSpec) DeepCopyInto(out *ClusterLocalizationSpec) {
+	*out = *in
+	if in.Overrides != nil {
+		in, out := &in.Overrides, &out.Overrides
+		*out = make([]OverrideRule, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	in.Feed.DeepCopyInto(&out.Feed)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ClusterLocalizationSpec.
+func (in *ClusterLocalizationSpec) DeepCopy() *ClusterLocalizationSpec {
+	if in == nil {
+		return nil
+	}
+	out := new(ClusterLocalizationSpec)
 	in.DeepCopyInto(out)
 	return out
 }
@@ -107,6 +274,11 @@ func (in *DescriptionSpec) DeepCopyInto(out *DescriptionSpec) {
 	if in.Charts != nil {
 		in, out := &in.Charts, &out.Charts
 		*out = make([]ChartReference, len(*in))
+		copy(*out, *in)
+	}
+	if in.Raw != nil {
+		in, out := &in.Raw, &out.Raw
+		*out = make([]byte, len(*in))
 		copy(*out, *in)
 	}
 	return
@@ -359,6 +531,113 @@ func (in *HelmReleaseStatus) DeepCopy() *HelmReleaseStatus {
 		return nil
 	}
 	out := new(HelmReleaseStatus)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *Localization) DeepCopyInto(out *Localization) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	in.Spec.DeepCopyInto(&out.Spec)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new Localization.
+func (in *Localization) DeepCopy() *Localization {
+	if in == nil {
+		return nil
+	}
+	out := new(Localization)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *Localization) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *LocalizationList) DeepCopyInto(out *LocalizationList) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
+	if in.Items != nil {
+		in, out := &in.Items, &out.Items
+		*out = make([]Localization, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new LocalizationList.
+func (in *LocalizationList) DeepCopy() *LocalizationList {
+	if in == nil {
+		return nil
+	}
+	out := new(LocalizationList)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *LocalizationList) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *LocalizationSpec) DeepCopyInto(out *LocalizationSpec) {
+	*out = *in
+	if in.Overrides != nil {
+		in, out := &in.Overrides, &out.Overrides
+		*out = make([]OverrideRule, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	in.Feed.DeepCopyInto(&out.Feed)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new LocalizationSpec.
+func (in *LocalizationSpec) DeepCopy() *LocalizationSpec {
+	if in == nil {
+		return nil
+	}
+	out := new(LocalizationSpec)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *OverrideRule) DeepCopyInto(out *OverrideRule) {
+	*out = *in
+	if in.OverrideValue != nil {
+		in, out := &in.OverrideValue, &out.OverrideValue
+		*out = make(map[string]string, len(*in))
+		for key, val := range *in {
+			(*out)[key] = val
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new OverrideRule.
+func (in *OverrideRule) DeepCopy() *OverrideRule {
+	if in == nil {
+		return nil
+	}
+	out := new(OverrideRule)
 	in.DeepCopyInto(out)
 	return out
 }

clusternet/apis

apps/v1alpha1/zz_generated.deepcopy.go
@@ -25,6 +25,89 @@ import (
 )
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *Base) DeepCopyInto(out *Base) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	in.Spec.DeepCopyInto(&out.Spec)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new Base.
+func (in *Base) DeepCopy() *Base {
+	if in == nil {
+		return nil
+	}
+	out := new(Base)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *Base) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *BaseList) DeepCopyInto(out *BaseList) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
+	if in.Items != nil {
+		in, out := &in.Items, &out.Items
+		*out = make([]Base, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new BaseList.
+func (in *BaseList) DeepCopy() *BaseList {
+	if in == nil {
+		return nil
+	}
+	out := new(BaseList)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *BaseList) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *BaseSpec) DeepCopyInto(out *BaseSpec) {
+	*out = *in
+	if in.Feeds != nil {
+		in, out := &in.Feeds, &out.Feeds
+		*out = make([]Feed, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new BaseSpec.
+func (in *BaseSpec) DeepCopy() *BaseSpec {
+	if in == nil {
+		return nil
+	}
+	out := new(BaseSpec)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *ChartReference) DeepCopyInto(out *ChartReference) {
 	*out = *in
 	return
@@ -109,6 +192,11 @@ func (in *DescriptionSpec) DeepCopyInto(out *DescriptionSpec) {
 		*out = make([]ChartReference, len(*in))
 		copy(*out, *in)
 	}
+	if in.Raw != nil {
+		in, out := &in.Raw, &out.Raw
+		*out = make([]byte, len(*in))
+		copy(*out, *in)
+	}
 	return
 }
 
@@ -155,6 +243,90 @@ func (in *Feed) DeepCopy() *Feed {
 		return nil
 	}
 	out := new(Feed)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *Globalization) DeepCopyInto(out *Globalization) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	in.Spec.DeepCopyInto(&out.Spec)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new Globalization.
+func (in *Globalization) DeepCopy() *Globalization {
+	if in == nil {
+		return nil
+	}
+	out := new(Globalization)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *Globalization) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *GlobalizationList) DeepCopyInto(out *GlobalizationList) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
+	if in.Items != nil {
+		in, out := &in.Items, &out.Items
+		*out = make([]Globalization, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new GlobalizationList.
+func (in *GlobalizationList) DeepCopy() *GlobalizationList {
+	if in == nil {
+		return nil
+	}
+	out := new(GlobalizationList)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *GlobalizationList) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *GlobalizationSpec) DeepCopyInto(out *GlobalizationSpec) {
+	*out = *in
+	if in.Overrides != nil {
+		in, out := &in.Overrides, &out.Overrides
+		*out = make([]OverrideRule, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	in.Feed.DeepCopyInto(&out.Feed)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new GlobalizationSpec.
+func (in *GlobalizationSpec) DeepCopy() *GlobalizationSpec {
+	if in == nil {
+		return nil
+	}
+	out := new(GlobalizationSpec)
 	in.DeepCopyInto(out)
 	return out
 }
@@ -359,6 +531,113 @@ func (in *HelmReleaseStatus) DeepCopy() *HelmReleaseStatus {
 		return nil
 	}
 	out := new(HelmReleaseStatus)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *Localization) DeepCopyInto(out *Localization) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	in.Spec.DeepCopyInto(&out.Spec)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new Localization.
+func (in *Localization) DeepCopy() *Localization {
+	if in == nil {
+		return nil
+	}
+	out := new(Localization)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *Localization) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *LocalizationList) DeepCopyInto(out *LocalizationList) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
+	if in.Items != nil {
+		in, out := &in.Items, &out.Items
+		*out = make([]Localization, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new LocalizationList.
+func (in *LocalizationList) DeepCopy() *LocalizationList {
+	if in == nil {
+		return nil
+	}
+	out := new(LocalizationList)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *LocalizationList) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *LocalizationSpec) DeepCopyInto(out *LocalizationSpec) {
+	*out = *in
+	if in.Overrides != nil {
+		in, out := &in.Overrides, &out.Overrides
+		*out = make([]OverrideRule, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	in.Feed.DeepCopyInto(&out.Feed)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new LocalizationSpec.
+func (in *LocalizationSpec) DeepCopy() *LocalizationSpec {
+	if in == nil {
+		return nil
+	}
+	out := new(LocalizationSpec)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *OverrideRule) DeepCopyInto(out *OverrideRule) {
+	*out = *in
+	if in.OverrideValue != nil {
+		in, out := &in.OverrideValue, &out.OverrideValue
+		*out = make(map[string]string, len(*in))
+		for key, val := range *in {
+			(*out)[key] = val
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new OverrideRule.
+func (in *OverrideRule) DeepCopy() *OverrideRule {
+	if in == nil {
+		return nil
+	}
+	out := new(OverrideRule)
 	in.DeepCopyInto(out)
 	return out
 }